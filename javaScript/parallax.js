39cf2f2a45d247033826770e49d209ab90acf3c9
Bjørn Emmaneel <27728@ma-web.nl>
2019-10-15 11:51:31 +0200

    update

--- javaScript/parallax.js
+++ javaScript/parallax.js
@@ -2,33 +2,43 @@ let scrollAfstand;
 let sectie2 = document.getElementsByClassName('sectie--2')[0];
 let sectie4 = document.getElementsByClassName('sectie--4')[0];
 let sectie6 = document.getElementsByClassName('sectie--6')[0];
+let indicator = document.getElementById('indicator');
+const documentHoogte = document.body.clientHeight;
+const viewPoortHoogte = window.innerHeight;
 
-window.addEventListener('scroll', (e)=>{
+window.addEventListener('scroll', (e) => {
     scrollAfstand = window.pageYOffset;
     // console.log(scrollAfstand);
     corrSection2(scrollAfstand);
     corrSection4(scrollAfstand);
     corrSection6(scrollAfstand);
-    if( scrollAfstand >= 938){
+    if (scrollAfstand >= 938) {
         verwijderClassKop();
+    } else {
+        voegclassKopToe();
     }
+    werkIndicatorBij(scrollAfstand);
 });
 
 // Sectie 2 wordt hier gestyled wanneer er gescrolled word
 const corrSection2 = (gescrolled) => {
-    sectie2.style.backgroundPositionY = 20 -gescrolled/8 + "px";
+    sectie2.style.backgroundPositionY = 20 - gescrolled / 8 + "px";
     // De kop schijft van links naar rechts na weg springen.
-    sectie2.getElementsByClassName('section__kop')[0].style.marginLeft = -300+gescrolled+2 + 'px';
+    sectie2.getElementsByClassName('section__kop')[0].style.marginLeft = -300 + gescrolled + 2 + 'px';
 };
-
 const corrSection4 = (gescrolled) => {
-    sectie4.style.backgroundPositionY = 100 -gescrolled/8 + "px";
+    sectie4.style.backgroundPositionY = 100 - gescrolled / 8 + "px";
 };
-
 const corrSection6 = (gescrolled) => {
-    sectie6.style.backgroundPositionY = 200 -gescrolled/8 + "px";
+    sectie6.style.backgroundPositionY = 200 - gescrolled / 8 + "px";
 };
-
 const verwijderClassKop = () => {
     document.querySelector('.sectie--4 h2').classList.remove('sectie--4__kop');
+};
+const voegclassKopToe = () => {
+    document.querySelector('.sectie--4 h2').classList.add('sectie--4__kop');
+};
+
+const werkIndicatorBij = (getal) => {
+    indicator.style.width = 100 * getal / (documentHoogte - viewPoortHoogte) + '%';
 };
